ce1f1713fba5b61b9e08c321646b5fceb2e5bc8e
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-04-24 00:45:54 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function HomePage() {
       <Container>
         <div className="space-y-6">
           <h1 className="text-2xl font-bold">
-            Hey, I'm Ata Tolga (you know me as Whattyu). I enjoy working with Next.js and JavaScript Discord bots.
+            Hey there 🙋, I'm Ata Tolga (you know me as Whattyu). I enjoy working with Next.js and JavaScript Discord bots.
           </h1>
           <h2>
           Some of my Projects ⬎
