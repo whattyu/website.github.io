19a117d3773057f6da67f4b04d84a14443a6aace
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-04-24 00:42:37 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ function HomePage() {
           <h1 className="text-2xl font-bold">
             Hey, I'm Ata Tolga (you know me as Whattyu). I enjoy working with Next.js and JavaScript Discord bots.
           </h1>
-          <p>
+          <h2>
           Some of my Projects ⬎
-          </p>
+          </h2>
           <ul>
             <li>My First Bot -> <a href="https://cariyebot.github.io/">Cariye Bot</a> </li>
             <li>My Second (and best) Bot -> <a href="https://top.gg/bot/849663572308918343/">Cariye+ Bot</a> </li>
