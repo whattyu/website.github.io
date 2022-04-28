f08929a64e799949415152a69c022734862ab650
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-04-28 23:22:44 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,15 @@ function HomePage() {
           <h2>
           Some of my Projects ⬎
           </h2>
-          <ul>
-            <li>My First Bot -> <a href="https://cariyebot.github.io/">Cariye Bot</a> </li>
-            <li>My Second (and best) Bot -> <a href="https://top.gg/bot/849663572308918343/">Cariye+ Bot</a> </li>
-            <li>My Basic Calculator <a href="https://calculate.whattyu.repl.co/">Site</a> </li>
-            <li>My Basic <a href="https://tetris-game.whattyu.repl.co/">Tetris Game</a> </li>
-            <li>My Basic Python <a href="https://tetris-game.whattyu.repl.co/">Projects</a> </li>
-          </ul>
+          <p>
+            <ul>
+              <li>My First Bot -> <a href="https://cariyebot.github.io/">Cariye Bot</a> </li>
+              <li>My Second (and best) Bot -> <a href="https://top.gg/bot/849663572308918343/">Cariye+ Bot</a> </li>
+              <li>My Basic Calculator <a href="https://calculate.whattyu.repl.co/">Site</a> </li>
+              <li>My Basic <a href="https://tetris-game.whattyu.repl.co/">Tetris Game</a> </li>
+              <li>My Basic Python <a href="https://tetris-game.whattyu.repl.co/">Projects</a> </li>
+            </ul>
+          </p>
         </div>
       </Container>
     </>
