3316b51212218ee62f2fac19069225d7ba08b5fe
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-05-05 21:18:50 +0300

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -17,7 +17,7 @@ export default function PostPage({ post }) {
   return (
     <Container>
       <Head>
-        <title>{post.title} | My awesome blog</title>
+        <title>{post.title} | Posts</title>
       </Head>
 
       {router.isFallback ? (
