11a01a26a4ea080911b7703e0d5864cdd2f1d9b7
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-12-20 21:53:51 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,11 @@ function HomePage() {
           </h2>
           <p>
             <ul>
-              <li>My First Bot -> <a href="https://cariyebot.github.io/">Cariye Bot</a> </li>
-              <li>My Second (and best) Bot -> <a href="https://top.gg/bot/849663572308918343/">Cariye+ Bot</a> </li>
-              <li>My Basic Calculator <a href="https://calculate.whattyu.repl.co/">Site</a> </li>
-              <li>My Basic <a href="https://tetris-game.whattyu.repl.co/">Tetris Game</a> </li>
-              <li>My Basic Python <a href="https://tetris-game.whattyu.repl.co/">Projects</a> </li>
+              <li>My First Bot -> <a href="https://cariyebot.github.io/" target="_blank">Cariye Bot</a> </li>
+              <li>My Second (and best) Bot -> <a href="https://top.gg/bot/849663572308918343/" target="_blank">Cariye+ Bot</a> </li>
+              <li>My Basic Calculator <a href="https://calculate.whattyu.repl.co/" target="_blank">Site</a> </li>
+              <li>My Basic <a href="https://tetris-game.whattyu.repl.co/" target="_blank">Tetris Game</a> </li>
+              <li>My Basic Python <a href="https://tetris-game.whattyu.repl.co/" target="_blank">Projects</a> </li>
             </ul>
           </p>
     </div>
@@ -27,4 +27,4 @@ function HomePage() {
   )
 }
 
-export default HomePage
+export default HomePage;
